dfa89f88d9130bcc92c3b91d5e44a3cf6560c6a9
emmanuel Mensah <emmaxp183@gmail.com>
2022-02-15 12:37:31 +0100

    Initial Commit

---
+++ InstagramUI/InstagramUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramUIApp.swift
+//  InstagramUI
+//
+//  Created by admin on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstagramUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
